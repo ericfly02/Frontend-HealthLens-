7606da2075c96922d181e8314b7471d02bb1c78e
EricGd <speedyand@gmail.com>
2024-10-15 19:30:26 +0200

    Update MainPage.tsx

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -60,7 +60,6 @@ export default function MainPage() {
   
     try {
       const response = await axios.post(apiUrl, formData,  {
-        withCredentials: true, 
         headers: { 'Content-Type': 'multipart/form-data' },
       });
   
