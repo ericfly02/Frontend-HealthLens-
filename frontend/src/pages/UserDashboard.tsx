9ea9830121742ac9c08cd3a5139b0be402e0acaa
EricGd <speedyand@gmail.com>
2024-10-16 19:35:00 +0200

    Revert "fixed errors"
    This reverts commit ff84e5387259060f92be1e6106e8d92a757c70b0.

--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -21,7 +21,6 @@ interface UserData {
   weight: number;
   sex: string;
   scans: number;
-  diseases: string[];
   //recentSymptoms: string[];
 }
 
@@ -40,8 +39,6 @@ export default function UserDashboard() {
     const fetchUserData = async () => {
       try {
         const token = localStorage.getItem('token'); // Assuming the token is stored in localStorage
-
-        
 
         const response = await axios.get('https://backend-health-lens.vercel.app/user/me', {
           headers: {
@@ -73,11 +70,8 @@ export default function UserDashboard() {
         weight: userData?.weight,
         sex: userData?.sex,
         scans: userData?.scans,
-        diseases: userData?.diseases,
         //recentSymptoms: userData?.recentSymptoms,
       };
-
-      console.log("Diseases: ", reportData.diseases);
     
       await axios.post('https://backend-health-lens.vercel.app/user/email-report', reportData, {
         headers: {
